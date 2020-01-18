eb69084c4d3e418bcba5cd6f17420249f9abc2aa
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-18 15:04:01 -0800

    method overriding

---
+++ prototypical-inheritance/method-overriding.js
@@ -0,0 +1,38 @@
+
+
+
+//create a reusable inheritance function == intermediate function inheritance
+function extend(Child, Parent){
+    //create inheritance
+    Child.prototype = Object.create(Parent.prototype)
+    //reset constructor
+    Child.prototype.constructor = Child
+}
+
+
+function Shape(color){
+}
+
+
+Shape.prototype.duplicate = function(){
+    console.log('duplicate')
+}
+
+
+//we initialize the shape here with the circle constructor
+function Circle(){
+}
+
+extend(Circle, Shape)
+
+//overrid ethe duplicate method for circle == must be after we extend circle to shape
+//javascript will now first look to the circle object (child object) by default and call duplicate on it
+Circle.prototype.duplicate = function(){
+    Shape.prototype.duplicate.call(this) //if you use 'this' within the funciton. note not necessary here 
+    //Shape.prototype.duplicate() could use this if you don't use 'this' within the function
+    console.log('duplicate circle')
+}
+
+
+const c = new Circle()
+c.duplicate()
