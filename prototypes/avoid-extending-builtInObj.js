f0f4ab2986ee02d75c57c60ecd97ae358e22f5a8
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-18 10:57:27 -0800

    avoid extenidng built in objects

---
+++ prototypes/avoid-extending-builtInObj.js
@@ -0,0 +1,12 @@
+
+
+//dont do this!!!!!!!!!!!!!!
+//built in objects should not be modified
+//do not modify objects you do not own
+Array.prototype.shuffle = function(){
+    console.log('gose')
+}
+
+const array = []
+array.shuffle()
+
