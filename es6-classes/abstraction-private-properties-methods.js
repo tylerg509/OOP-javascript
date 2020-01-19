5cbe54ccc6c33349f62456f4e22e2e33f1094119
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-19 06:24:28 -0800

    private properties methods

---
+++ es6-classes/abstraction-private-properties-methods.js
@@ -0,0 +1,27 @@
+
+//abstraction = use private whenever possible 
+//we use symbols to implement private properties 
+const _radius = Symbol(); //symbol is a unique id 
+const _draw = Symbol();
+class Circle {
+    constructor(radius){
+        this[_radius] = radius //here we use the symbol as the property name 
+        ////could also use these
+        // this['radius'] = radius
+        // this[_radius]= radius
+    }
+    //private method
+    [_draw](){
+
+    }
+}
+
+const c = new Circle(73);
+//get value from symbol
+const key = Object.getOwnPropertySymbols(c)[0]
+console.log(c[key])
+
+
+console.log(c)
+//c.  note how you cannot access this[_radius] or draw
+
